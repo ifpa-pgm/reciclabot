6a581333ff5b2434c2d61d1cfd98fbd903aff3c3
ifpa-pgm <rafael-tdr@hotmail.com>
2022-11-22 07:09:27 -0300

    Update codigo.ino

--- examples/codigo.ino
+++ examples/codigo.ino
@@ -1,11 +1,7 @@
 #include <ReciclaBot.h>
 
 ReciclaBot robo;
-/*
- 1 - Buscar inimigo
- 2 - Atacar
-*/
-int estado = 0;
+
 void setup() {
   robo.configurarPinoSensorLinha(3);
   robo.configurarPinoSensorDistancia(13, 12);
@@ -15,33 +11,25 @@ void setup() {
 }
 
 void loop() {
-  if (robo.refletiuLuz()) {//VOLTAR P ARENA
+  if (robo.refletiuLuz()) {  //Voltar para arena
     robo.recuar(255);
     delay(100);
-  }
-
-
-  if (estado == 1) {  //busca inimigo
-    robo.avancar(120);
-    delay(100);
-
-    robo.girar(200);
-    delay(100);
-
-    robo.avancar(120);
-    delay(100);
-
-    robo.girar(-200);
-    delay(100);
-  }
-  if (robo.distancia() <= 20) {  // Achou inimigo
-    estado = 2;
-  } else {  // continuar procurando
-    estado = 1;
-  }
-
-  if (estado == 2) {  //Atacar
+    
+  } else if (robo.distancia() <= 20) {  //atacar
     robo.avancar(255);
     delay(100);
+
+  } else {  //busca inimigo
+    robo.avancar(120);
+    delay(200);
+
+    robo.girar(200);
+    delay(200);
+
+    robo.avancar(120);
+    delay2(0);
+
+    robo.girar(-200);
+    delay(200);
   }
 }
